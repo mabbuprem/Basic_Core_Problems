1dbddff8b7015085101e410e739e9148d85da888
mabbuprem <mabbupremkumar@gmail.com>
2022-02-15 20:07:04 +0530

    Percentage Flip Coin

--- Program.cs
+++ Program.cs
@@ -11,42 +11,22 @@ namespace Basic
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to basic Core Programms");
-            LeapYear Y1 = new LeapYear(1500);
-            Y1.CheckLeapYear();
+            //CONSTANTS
+            const int MAXNUMBEROFFLIPPINGCOIN = 20;
+            //VARIABLES
+            int noOfFlipping = 0;
+            int tailCounter = 0;
+            int headCounter = 0;
+
+            while (noOfFlipping < MAXNUMBEROFFLIPPINGCOIN)
+            {
+                Random random = new Random();
+                double flipCoin = random.NextDouble();
+                int result = flipCoin < 0.5 ? tailCounter++ : headCounter++;
+                noOfFlipping++;
+            }
+            Console.WriteLine($"The percentage of HEAD winning is {headCounter * 100 / MAXNUMBEROFFLIPPINGCOIN}");
+            Console.WriteLine($"The percentage of TAIL winning is {tailCounter * 100 / MAXNUMBEROFFLIPPINGCOIN}");
         }
     }
-    public class LeapYear
-    {
-        public int year;
-
-        public LeapYear(int year)
-        {
-            this.year = year;
-        }
-        public void CheckLeapYear()
-        {
-            if (year > 1000)
-            {
-
-                if (year % 400 == 0 && year % 100 == 0)
-                {
-                    Console.WriteLine($"{year} is a leap year");
-                }
-                else if (year % 4 == 0 && year % 100 != 0)
-                {
-                    Console.WriteLine($"{year} is a leap year");
-                }
-                else
-                {
-                    Console.WriteLine($"{year} is not a leap year");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Please enter a four digit Number");
-            }
-        }
-
-    }
-
 }
