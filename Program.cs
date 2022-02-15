a1e49670dbf5ab7e94ff480e3f458c5066fc6033
mabbuprem <mabbupremkumar@gmail.com>
2022-02-15 20:28:32 +0530

    Largesr of 3 numbers

--- Program.cs
+++ Program.cs
@@ -10,22 +10,37 @@ namespace Basic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to basic Core Programms");
-            Console.Write("Enter the value of N: ");
-            //VARIABLES
-            int N = Convert.ToInt32(Console.ReadLine());
-            int i = 1;
-            float nthHarmonicValue = 0;
-
-            while (i <= N)
-            {
-                Console.Write($"1/{i} + ");
-                nthHarmonicValue += 1 / (float)i;
-                i++;
-            }
-            Console.WriteLine($"\n{nthHarmonicValue}");
+            Console.WriteLine("Welcome to basic Core Programs");
+            numbers N1 = new numbers(200, 546, 955);
+            N1.LargestNumber();
         }
+        public class numbers
+        {
+            int number1;
+            int number2;
+            int number3;
 
+            public numbers(int number1, int number2, int number3)
+            {
+                this.number1 = number1;
+                this.number2 = number2;
+                this.number3 = number3;
+            }
+            public void LargestNumber()
+            {
+                if (number1 > number2 && number1 > number3)
+                {
+                    Console.WriteLine($"{number1} is largest");
+                }
+                else if (number2 > number1 && number2 > number3)
+                {
+                    Console.WriteLine($"{number2} is largest");
+                }
+                else
+                {
+                    Console.WriteLine($"{number3} is largest");
+                }
+            }
+        }
     }
-
 }
