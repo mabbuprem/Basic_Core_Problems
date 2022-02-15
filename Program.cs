8734fe37c475f3e85fb5db1d8b494ee6f643bb70
mabbuprem <mabbupremkumar@gmail.com>
2022-02-15 20:44:10 +0530

    swap 2 numbers

--- Program.cs
+++ Program.cs
@@ -11,42 +11,16 @@ namespace Basic
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to basic Core Programms");
-            Console.Write("Enter a number to get prime factors: ");
-            int primeFactorNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(PrimeFactors(primeFactorNumber));
-        }
-        public static string PrimeFactors(int Number)
-        {
-            int i = 2;
-            string rv = "";
+            Console.Write("Enter 1st number: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter 2nd number: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int x;
+            x = number1;
+            number1 = number2;
+            number2 = x;
+            Console.WriteLine($"Numbers after swapping:-\nnumber1:{number1} & number2: {number2}");
 
-            while (i * i < Number)
-            {
-                if (Number % i == 0)
-                {
-                    int n = 2;
-                    int flag = 0;
-                    while (n < i)
-                    {
-                        if (i % n == 0)
-                        {
-                            flag = 1;
-                            break;
-                        }
-                        else
-                        {
-                            flag = 0;
-                        }
-                        n++;
-                    }
-                    if (flag == 0)
-                    {
-                        rv = $"{Convert.ToString(i)} {rv}";
-                    }
-                }
-                i++;
-            }
-            return rv;
         }
     }
 }
