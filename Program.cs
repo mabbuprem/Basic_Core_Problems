d5e319ca036a8518ed318145ee09554fc31c7bea
mabbuprem <mabbupremkumar@gmail.com>
2022-02-15 20:40:12 +0530

    Prime Factor

--- Program.cs
+++ Program.cs
@@ -11,16 +11,42 @@ namespace Basic
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to basic Core Programms");
+            Console.Write("Enter a number to get prime factors: ");
+            int primeFactorNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(PrimeFactors(primeFactorNumber));
+        }
+        public static string PrimeFactors(int Number)
+        {
+            int i = 2;
+            string rv = "";
 
-            Console.Write("Enter a number to get quotient & reminder: ");
-            int number = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter a number by which Number is to be divide: ");
-            int dividingNumber = Convert.ToInt32(Console.ReadLine());
-
-            int quotient = number / dividingNumber;
-            int reminder = number % dividingNumber;
-
-            Console.WriteLine($"The quotient is {quotient} & the reminder is {reminder}");
+            while (i * i < Number)
+            {
+                if (Number % i == 0)
+                {
+                    int n = 2;
+                    int flag = 0;
+                    while (n < i)
+                    {
+                        if (i % n == 0)
+                        {
+                            flag = 1;
+                            break;
+                        }
+                        else
+                        {
+                            flag = 0;
+                        }
+                        n++;
+                    }
+                    if (flag == 0)
+                    {
+                        rv = $"{Convert.ToString(i)} {rv}";
+                    }
+                }
+                i++;
+            }
+            return rv;
         }
     }
 }
